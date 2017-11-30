fc8159302c57d7564629f1709e7cb387dca1cdff
Jean-Philippe Coene <jean-philippe.coene@weforum.org>
2017-11-30 14:20:33 +0100

    footer

--- R/bulma-footer.R
+++ R/bulma-footer.R
@@ -1,30 +1,60 @@
 #' Add a footer
 #'
 #' @inheritParams bulmaPage
+#' @param centered wheter to center the text.
 #'
 #' @examples
 #' library(shiny)
-#' shinyApp(
-#'   ui = bulmaPage(
-#'     bulmaFooter(
-#'       shiny::p(
+#'
+#' ui <- bulmaPage(
+#'   bulmaHero(
+#'     fullheight = TRUE,
+#'     color = "primary",
+#'     bulmaHeroHead(
+#'       bulmaContainer(
+#'         bulmaTitle("Hero Title")
+#'       )
+#'     ),
+#'     bulmaHeroHead(
+#'       bulmaContainer(
+#'         bulmaSubtitle(
+#'           "Some content"
+#'         )
+#'       )
+#'     ),
+#'     bulmaHeroFoot(
+#'       bulmaContainer(
 #'         "The footer"
 #'       )
 #'     )
-#'   ),
-#'   server = function(input, output) {}
+#'   )
 #' )
 #'
+#' server <- function(input, output){}
+#'
+#' shinyApp(ui, server)
+#'
 #' @export
-bulmaFooter <- function(..., container = TRUE){
+bulmaFooter <- function(..., container = TRUE, centered = TRUE){
+
+  cl <- "content"
+
+  if(isTRUE(centered)) cl <- paste(cl, "has-text-centered")
+
   shiny::tags$footer(
     if(isTRUE(container)){
       tags$div(
         class = "container",
-        ...
+        shiny::tags$div(
+          class = cl,
+          ...
+        )
       )
     } else {
-      ...
+      shiny::tags$div(
+        class = cl,
+        ...
+      )
     }
   )
 }
