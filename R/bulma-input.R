398c615f58a55eda750bd162c33bc7db3cd012cd
Divad0175 <dgranjon@ymail.com>
2018-04-12 14:29:20 +0200

    add bulmaSwitchInput to the bulma-input.R database.

--- R/bulma-input.R
+++ R/bulma-input.R
@@ -245,3 +245,162 @@ bulmaActionButton <- function(inputId, label, color = NULL){
     button
   )
 }
+
+
+
+#' @title Add switch input
+#'
+#' @author David Granjon, \email{dgranjon@@ymail.com}
+#'
+#' @description Create a switch input \url{https://wikiki.github.io/form/switch/}.
+#'
+#' @param inpuId The input slot that will be used to access the value.
+#' @param label Switch label.
+#' @param value TRUE or FALSE, FALSE by default.
+#' @param color Switch color : \code{link}, \code{info}, \code{primary}, \code{warning},
+#'  \code{danger}, \code{success}, \code{black}, \code{dark} and \code{ligth}.
+#' @param size Switch size : \code{small}, \code{medium} and \code{large}.
+#' @param style Switch style :  \code{thin}, \code{rounded}, \code{outlined} or 
+#' \code{mixed1}, \code{mixed2}, \code{mixed3} and \code{mixed4} (mixed1 is thin-rounded, mixed2
+#' is thin-outlined, mixed3 is rounded-outlined and mixed4 is thin-rounded-outlined)
+#' NULL by default, which corresponds to squared style.
+#' 
+#'
+#' @export
+#'
+#' @examples
+#' if(interactive()) {
+#' library(shiny)
+#' 
+#' shinyApp(
+#'  ui = bulmaNavbarPage(
+#'    bulmaNavbar(
+#'      bulmaNavbarBrand(
+#'        bulmaNavbarItem(
+#'          "shinybulma"
+#'        ),
+#'        bulmaNavbarBurger()
+#'      ),
+#'      bulmaNavbarMenu( # not visible on smaller devices
+#'        bulmaNavbarItem(
+#'          "Switch Inputs"
+#'        )
+#'      )
+#'    ),
+#'    bulmaNav(
+#'      "Switch Inputs",
+#'      div(style = "text-align: center;",
+#'          bulmaTitle("The plot only displays if switch 1 is TRUE.")),
+#'      
+#'      br(), br(), hr(),
+#'      
+#'      bulmaContainer(
+#'        bulmaColumns(
+#'          bulmaColumn(
+#'            width = 6,
+#'            bulmaSubtitle("Inputs"),
+#'            bulmaSwitchInput(
+#'              inputId = "switch1", 
+#'              label = "switch 1", 
+#'              value = FALSE,
+#'              color = NULL, 
+#'              size = NULL, 
+#'              style = NULL,
+#'              rtl = FALSE, 
+#'              disabled = FALSE),
+#'            
+#'            bulmaSwitchInput(
+#'              inputId = "switch2", 
+#'              label = "switch 2", 
+#'              value = TRUE,
+#'              color = "warning", 
+#'              size = "large", 
+#'              style = "rounded",
+#'              
+#'              rtl = FALSE, 
+#'              disabled = TRUE),
+#'            
+#'            bulmaSwitchInput(
+#'              inputId = "switch3", 
+#'              label = "switch 3", 
+#'              value = TRUE,
+#'              
+#'              color = "danger", 
+#'              size = "small", 
+#'              style = "thin",
+#'              
+#'              rtl = FALSE, 
+#'              disabled = FALSE),
+#'            
+#'            bulmaSwitchInput(
+#'              inputId = "switch4", 
+#'              label = "switch 4", 
+#'              value = FALSE,
+#'              
+#'              color = NULL, 
+#'              size = NULL, 
+#'              style = "mixed3",
+#'              rtl = FALSE, 
+#'              disabled = FALSE)
+#'          ),
+#'          bulmaColumn(
+#'            width = 6,
+#'            bulmaSubtitle("Outputs"),
+#'            plotOutput("plot1"),
+#'            uiOutput("switches")
+#'          )
+#'        )
+#'      )
+#'    )
+#'  ),
+#'  server = function(input, output) {
+#'    
+#'    output$plot1 <- renderPlot({
+#'      if (input$switch1 == TRUE) {
+#'        plot(mtcars$wt, mtcars$mpg)
+#'      }
+#'    })
+#'    
+#'    output$switches <- renderPrint({
+#'      c(input$switch1, input$switch2, input$switch3, input$switch4)
+#'    })
+#'  }
+#'  )
+#' }
+
+bulmaSwitchInput <- function(inputId, label = NULL, value = FALSE, 
+                             color = NULL, size = NULL, style = NULL, 
+                             rtl = FALSE, disabled = FALSE) {
+  
+  cl <- "switch shinyBulmaSwitch"
+  if (!is.null(color)) cl <- paste0(cl, " is-", color)
+  if (!is.null(size)) cl <- paste0(cl, " is-", size)
+  if (!is.null(style)) {
+    if (style == "mixed1") cl <- paste0(cl, " is-thin is-rounded")
+    else if (style == "mixed2") cl <- paste0(cl, " is-thin is-outlined")
+    else if (style == "mixed3") cl <- paste0(cl, " is-rounded is-outlined")
+    else if (style == "mixed4") cl <- paste0(cl, " is-thin is-rounded is-outlined")
+    else cl <- paste0(cl, " is-", style)
+  }
+  
+  if (rtl == TRUE) cl <- paste0(cl, " is-rtl")
+  
+  field <- shiny::tags$div(
+    class = "field"
+  )
+  
+  switchInputTag <- shiny::tags$input(
+    class = cl, 
+    id = inputId, 
+    name = inputId,
+    type = "checkbox",
+    shiny::tags$label(`for` = inputId, label)
+  )
+  
+  if (!is.null(value) && value) switchInputTag$attribs$checked <- "checked"
+  if (!is.null(disabled) && disabled) switchInputTag$attribs$disabled <- NA
+  
+  field <- shiny::tagAppendChild(field, switchInputTag)
+  
+  field
+}
