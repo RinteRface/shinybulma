b253475f416b620446d8a0cee91d3ff4d23b13a9
John Coene <jcoenep@gmail.com>
2018-04-15 15:32:45 +0200

    page loader init TODO

---
+++ R/bulma-loading.R
@@ -0,0 +1,24 @@
+#' Show loading page
+#' 
+#' @export
+bulmaLoading <- function(title = "Loading", class = NULL){
+  cl <- paste("pageloader", class)
+  tags <- shiny::tags$div(
+    class = cl,
+    shiny::tags$span(
+      class = "title",
+      title
+    )
+  )
+  
+  shiny::tagList(
+    shiny::singleton(
+      shiny::tags$head(
+        shiny::includeCSS(
+          system.file(file.path("css", "bulma-pageloader.min.css"), package = "shinybulma")
+        )
+      )
+    ),
+    tags
+  )
+}
