c28cab742a9c4e99931eb3f12fa9ba48a62a208d
John Coene <jcoenep@gmail.com>
2018-04-22 20:51:10 +0200

    theme tweak + navbar show on doc ready

--- inst/js/custom-js.js
+++ inst/js/custom-js.js
@@ -1,16 +1,19 @@
-var first_id = $(".navTab").first().attr('id'); // first element
 
-$('#' + first_id).siblings('.navTab').each(function () {
-   $(this).hide();
-   $(this).trigger('hide');
-   $(this).trigger('hidden');
-   $(this).removeClass('is-active');
-   $(this).css("display", "none");
+$(function() {
+  var first_id = $(".navTab").first().attr('id'); // first element
+  
+  $('#' + first_id).siblings('.navTab').each(function () {
+     $(this).hide();
+     $(this).trigger('hide');
+     $(this).trigger('hidden');
+     $(this).removeClass('is-active');
+     $(this).css("display", "none");
+  });
+  
+  // show first
+  $("#" + first_id).show();
+  $("#" + first_id).trigger('show');
+  $("#" + first_id).trigger('shown');
+  $("#" + first_id).addClass('is-active');
+  $("#" + first_id).css("display", "block");
 });
-
-// show first
-$("#" + first_id).show();
-$("#" + first_id).trigger('show');
-$("#" + first_id).trigger('shown');
-$("#" + first_id).addClass('is-active');
-$("#" + first_id).css("display", "block");
