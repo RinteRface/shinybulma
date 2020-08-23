267109241ea567bdd92b8b3cdcc86ecc09638f1e
DivadNojnarg <dgranjon@ymail.com>
2020-08-23 23:46:40 +0200

    bulma action button listens to the classic shiny action button input

--- R/bulma-input.R
+++ R/bulma-input.R
@@ -289,7 +289,7 @@ bulmaSelectInput <- function(inputId, label, choices, rounded = FALSE){
 #' @export
 bulmaActionButton <- function(inputId, label, color = NULL){
 
-  cl <- "button shinyBulmaActionButton"
+  cl <- "button action-button"
 
   if(!is.null(color)) cl <- paste0(cl, " is-", color)
 
