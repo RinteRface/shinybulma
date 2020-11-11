fdf3fd66c625793424f5db299ae642bf094ef3b0
Thorn Thaler <t.thaler@crifbuergel.de>
2020-11-11 12:56:24 +0100

    Fix not shown output elments upon tab change
    The default behaviour of shiny is not to show output elements
    (.shiny-bound-output) on hidden parts. These lements are only shown when
    the respective part is shown. This is implemented by listening to the
    `shown` event, which needs to be triggered to inform Shiny that an
    element is now visible.
    
    The tab functionality is implemented using jquery ui. Since this
    libarary is of course not aware of the requirements of Shiny we have to
    trigger a `shown` event manually in case we change a tab. This allows
    Shiny to show output elements when needed.
    
    This closes #28.

--- inst/js-0.7.2/bulma-tabs-js.js
+++ inst/js-0.7.2/bulma-tabs-js.js
@@ -1,3 +1,7 @@
 $( function() {
   $( ".bulmaTabs" ).tabs();
+  $(".tabs li[role = 'tab']").on("click", function() {
+     var $content = $($(this).children("a").attr("href"));
+     $content.find(".shiny-bound-output").trigger("shown");
+  });
 } );
