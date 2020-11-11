77cea0268d1a073f105787a627844b2d1f04652d
Thorn Thaler <t.thaler@crifbuergel.de>
2020-11-11 16:32:24 +0100

    Fix missing hidden event triggering
    In an earlier commit we triggered the `shown` event when one clicks on
    the tab bar. In this commit we also trigger the `hidden` event for all
    tabs which are not selected.

--- inst/js-0.7.2/bulma-tabs-js.js
+++ inst/js-0.7.2/bulma-tabs-js.js
@@ -1,7 +1,10 @@
 $( function() {
   $( ".bulmaTabs" ).tabs();
   $(".tabs li[role = 'tab']").on("click", function() {
-     var $content = $($(this).children("a").attr("href"));
+     var $this = $(this);
+     var $content = $($this.children("a").attr("href"));
      $content.find(".shiny-bound-output").trigger("shown");
+     $this.siblings().children("a").each((_, el) => 
+         $($(el).attr("href")).find(".shiny-bound-output").trigger("hidden"));
   });
 } );
